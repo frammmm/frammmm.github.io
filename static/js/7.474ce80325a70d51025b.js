84cb54a7ba37e935c36e5b9647765d39ee33b3fd
Oleg Dolganov <frammm1337@gmail.com>
2019-04-17 23:15:31 +0500

    test novo

--- static/js/7.474ce80325a70d51025b.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([7],{wkqA:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l={render:function(){var e=this.$createElement;return(this._self._c||e)("h3",[this._v("Not found")])},staticRenderFns:[]},r=n("VU/8")(null,l,!1,null,null,null);t.default=r.exports}});
-//# sourceMappingURL=7.474ce80325a70d51025b.js.map
